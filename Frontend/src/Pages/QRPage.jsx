272953ff698fe8027f59d1ea66fdbfee7c6cb7c3
navjyotkapoor539 <159059683+navjyotkapoor539@users.noreply.github.com>
2024-12-17 23:17:49 +0530

    Update QRPage.jsx

--- Frontend/src/Pages/QRPage.jsx
+++ Frontend/src/Pages/QRPage.jsx
@@ -7,7 +7,7 @@ const QRPage=()=>{
     return (
         <div className="flex flex-col justify-center items-center h-screen bg-purple-800">
         <h1 className="text-2xl font-bold mb-4">Scan to View Menu</h1>
-        <QRCodeSVG value="http://localhost:5173/menu" size={200} className="mb-6" />
+        <QRCodeSVG value="https://qr-task-chi.vercel.app/menu" size={200} className="mb-6" />
         <Link to="/admin/login" className="text-black text-lg">
         Admin Login
         <img className="size-10 mx-auto" src={AdminIcon} alt="" />
